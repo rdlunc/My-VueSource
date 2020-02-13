f1b0e6e0cf25db5f0ceceb21125dc1e2af5455b7
Allen_glb <goodMorning_glb@rongda.com>
2020-02-14 01:14:13 +0800

    Vue_源码分析_数据代理_分析

--- js/mvvm/mvvm.js
+++ js/mvvm/mvvm.js
@@ -1,11 +1,19 @@
+/**
+ * 相当于Vue的构造函数
+ * @param options
+ * @constructor
+ */
 function MVVM(options) {
+    //将配置对象保存到vm
     this.$options = options;
+    //将data对象保存到vm和变量data中
     var data = this._data = this.$options.data;
+    //保存vm到变量me
     var me = this;
 
-    // 数据代理
-    // 实现 vm.xxx -> vm._data.xxx
-    Object.keys(data).forEach(function(key) {
+    //遍历data中所有的属性
+    Object.keys(data).forEach(function(key) { //key是data的某个属性名：name
+        //对指定的属性实现代理
         me._proxy(key);
     });
 
@@ -19,14 +27,23 @@ MVVM.prototype = {
         new Watcher(this, key, cb);
     },
 
+    /**
+     * 实现指定属性代理的方法
+     * @param key
+     * @private
+     */
     _proxy: function(key) {
+        //保存vm
         var me = this;
+        //给vm添加指定属性名的属性（使用的是属性描述符）
         Object.defineProperty(me, key, {
-            configurable: false,
-            enumerable: true,
+            configurable: false, //不能重新定义
+            enumerable: true, //可以枚举遍历
+            //当通过vm.xxx读取属性值时调用，从data中获取对应的属性值返回 【代理读操作】
             get: function proxyGetter() {
                 return me._data[key];
             },
+            //当通过vm.xxx = value 时，value被保存到data中对应的属性上 【代理写操作】
             set: function proxySetter(newVal) {
                 me._data[key] = newVal;
             }
